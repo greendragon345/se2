540b15a37e68937158300296c2e2f634d3c3346a
YoavNahumus <yoavnahum2005@gmail.com>
2022-10-06 23:39:15 +0300

    idk

--- se2/src/main/java/frc/robot/RobotContainer.java
+++ se2/src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.ShooterExperiment;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -22,10 +17,6 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
 
   /**
@@ -43,14 +34,20 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return new InstantCommand(()-> shoot.setV(5), shoot);
+    return null;
   }
   private static RobotContainer me;
  
-  private ShooterExperiment shoot;  
-  public RobotContainer() {
-    me=this;
-    shoot = new ShooterExperiment();
+  private ShooterExperiment shoot;
 
+  public static RobotContainer getContainer() {
+    if (me == null) {
+      me = new RobotContainer();
+    }
+    return me;
+  }
+
+  private RobotContainer() {
+    shoot = new ShooterExperiment();
   }
 }
--- se2/src/main/java/frc/robot/subsystems/ShooterExperiment.java
+++ se2/src/main/java/frc/robot/subsystems/ShooterExperiment.java
@@ -4,23 +4,28 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterExperiment extends SubsystemBase {
 
-    private TalonFX motor;
-    private PIDController PID;
+    private final TalonFX motor;
+    private final SimpleMotorFeedforward ff;
+    private double targetVelocity;
 
     public ShooterExperiment() {
         motor = new TalonFX(Constants.motorPortNum);
-        PID = new PIDController(Constants.kp, Constants.ki, Constants.kd);
+        motor.configFactoryDefault();
+        ff = new SimpleMotorFeedforward(Constants.shooterKs, Constants.shooterKv);
         motor.config_kD(0, Constants.kdMotor);
         motor.config_kP(0, Constants.kpMotor);
         motor.config_kI(0, Constants.kiMotor);
 
+        targetVelocity = 0;
     }
 
     public void setP(double p) {
@@ -28,21 +33,24 @@ public class ShooterExperiment extends SubsystemBase {
     }
 
     public void setV(double v) {
-        motor.set(ControlMode.Velocity, v*Constants.meterIndicatorS/10, 
-        DemandType.ArbitraryFeedForward, Constants.shooterKs*Math.signum(v)+Constants.shooterKv*v);
+        motor.set(ControlMode.Velocity, v*Constants.pulsesPerMeterS/10, 
+                DemandType.ArbitraryFeedForward, ff.calculate(v));
     }
 
     public double getVelocity() {
-        return motor.getSelectedSensorVelocity()/Constants.meterIndicatorS*10;
+        return motor.getSelectedSensorVelocity()/Constants.pulsesPerMeterS*10;
     }
 
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.addDoubleProperty("Velocity", this::getVelocity, null);
+        builder.addDoubleProperty("Error", motor::getClosedLoopError, null);
+        builder.addDoubleProperty("Output", motor::getMotorOutputPercent, null);
+        builder.addDoubleProperty("Setpoint", motor::getClosedLoopTarget, null);
 
+        builder.addDoubleProperty("Target Velocity", null, (num) -> {targetVelocity = num;});
+        SmartDashboard.putData(new RunCommand(() -> setV(targetVelocity), this));
 
-    public void periodic() {
-        SmartDashboard.putNumber("velocity", getVelocity());
+        super.initSendable(builder);
     }
-
-
-
-    
 }
